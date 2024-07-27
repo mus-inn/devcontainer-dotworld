3976348d3539d16d0af8686bdbdb9ec7385dac8c
gtko <gtko@users.noreply.github.com>
2024-07-27 00:17:31 +0000

    Update stubs tall

---
+++ stubs/stacks/Tall/custom_alias.sh
@@ -0,0 +1,4 @@
+
+alias phpunit="vendor/bin/phpunit"
+alias pest="vendor/bin/pest"
+alias pa="php artisan"
---
+++ stubs/stacks/Tall/custom_welcome.sh
@@ -0,0 +1,10 @@
+# Affiche le guide du développeur
+echo -e "\033[1;34mLe Guide Galactique du Développeur 2.0:\033[0m"
+echo -e "🔗 \033[1;33mhttps://tinyurl.com/guide-dev\033[0m - Ta bible chez dotworld"
+echo -e ""
+
+# Affiche le lien vers le README du projet
+echo -e "\033[1;34mLe Readme pour un developpeur au top !:\033[0m"
+echo -e "🔗 \033[1;33mREADME.md\033[0m - Ouvrir le fichier"
+
+echo -e ""
